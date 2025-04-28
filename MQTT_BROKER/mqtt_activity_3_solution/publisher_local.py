6c4d4b183b9c06a757b66bd502ed40597cd9d2da
solare <solar.elec.info@gmail.com>
2025-04-28 11:20:09 +0200

    adding UI version for MQTT general publisher

--- MQTT_BROKER/mqtt_activity_3_solution/publisher_local.py
+++ MQTT_BROKER/mqtt_activity_3_solution/publisher_local.py
@@ -1,52 +1,56 @@
 # simulator device 1 for mqtt message publishing
 import threading
-
 import paho.mqtt.client as mqtt
 import time
 import random
 import tkinter as tk
+import tkinter.scrolledtext as scrolledtext
 
 # Main layout definition
 main_layout = tk.Tk()
 main_layout.title("MQTT Temperature Publisher")
+main_layout.geometry("650x500")  # taille en pixel de l'écran
+log_box = None
 
 # Paramètres de connection au Broker(serveur MQTT)
-broker = tk.StringVar() #tkinder input variable "localhost"
-port = tk.IntVar() #tkinder input variable port mqtt
-keepalive = 60 # time to live(seconds)
-topic = f"telemetry/temp"# topic
-client_data = tk.StringVar() #"publisher:binome-raoult-pasteur"
+broker = tk.StringVar()  # tkinder input variable "localhost"
+port = tk.IntVar()  # tkinder input variable port mqtt
+keepalive = 60  # time to live(seconds)
+topic = f"telemetry/temp"  # topic
+client_data = tk.StringVar()  # "publisher:binome-raoult-pasteur"
+
+def log_message(msg):
+    # Utiliser after() pour mettre à jour l'interface depuis le thread principal
+    main_layout.after(0, lambda: log_box.insert(tk.END, msg + "\n"))
+    main_layout.after(0, lambda: log_box.see(tk.END))
 
 def submit_server_values():
     print("Broker:", broker.get())  # Récupère la valeur de broker
-    print("Port:", port.get())      # Récupère la valeur de port
+    print("Port:", port.get())  # Récupère la valeur de port
     print("keepalive:", keepalive)
     print("topic:", topic)
     print("client_data:", client_data.get())
 
-    #lance un serveur
+    # lance un serveur
     threading.Thread(target=publish_start_button, daemon=True).start()
 
 
-
 def on_publish(client, userdata, mid):
-    print(f"on_publish with userdata:{str(userdata)} and message id:{mid}")
-
+    log_message(f"on_publish with userdata:{str(userdata)} and message id:{mid}")
 
 
 def config_ui_interface():
-
-    #Définition de champs
+    # Définition de champs
     broker_host = tk.Label(main_layout, text="Broker:")
-    broker_host_entry = tk.Entry(main_layout,textvariable=broker)
+    broker_host_entry = tk.Entry(main_layout, textvariable=broker)
 
     broker_port = tk.Label(main_layout, text="Port:")
-    broker_port_entry = tk.Entry(main_layout,textvariable=port)
+    broker_port_entry = tk.Entry(main_layout, textvariable=port)
 
     broker_client_data = tk.Label(main_layout, text="Client_data:")
-    broker_client_data_entry = tk.Entry(main_layout,textvariable=client_data)
+    broker_client_data_entry = tk.Entry(main_layout, textvariable=client_data)
 
-    #Placement de champs dans le main_layout en mode FIFO
+    # Placement de champs dans le main_layout en mode FIFO
     broker_host.pack()
     broker_host_entry.pack()
 
@@ -60,8 +64,14 @@ def config_ui_interface():
     submit_button = tk.Button(main_layout, text="Valider", command=submit_server_values)
     submit_button.pack()
 
+    # Zone de text pour logs
+    global log_box #reference variable global
+    log_box = scrolledtext.ScrolledText(main_layout, width=60, height=20)
+    log_box.pack()
+
     # Lancer l'interface Tkinter
     main_layout.mainloop()
+
 
 def publish_start_button():
     client = mqtt.Client(userdata=client_data.get())
@@ -73,7 +83,7 @@ def publish_start_button():
         temp = round(random.randint(22, 30))
         temp = str(temp) + "°C"
         client.publish(topic, temp)
-        print(f"Température publiée sur {topic}:{temp}")
+        log_message(f"Température publiée sur {topic}:{temp}")
         time.sleep(4)
 
 
@@ -83,6 +93,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
