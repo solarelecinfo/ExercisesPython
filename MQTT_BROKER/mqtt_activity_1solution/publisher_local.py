6268dfcc98a3a0a4077552c2b66b90779ccf1178
solare <solar.elec.info@gmail.com>
2025-04-28 15:34:03 +0200

    final version of commands activity 4 et activyt 3

---
+++ MQTT_BROKER/mqtt_activity_1solution/publisher_local.py
@@ -0,0 +1,34 @@
+# simulator device 1 for mqtt message publishing
+import paho.mqtt.client as mqtt
+import time
+import random
+
+# Paramètres de connection au Broker(serveur MQTT)
+broker = "broker.hivemq.com" # hostname
+port = 1883 # port mqtt
+keepalive = 60 # time to live(seconds)
+topic = f"telemetry/temp"# topic
+# client internal data
+client_data="publisher:binome-raoult-pasteur"
+
+def on_publish(client, userdata, mid):
+    print(f"on_publish with userdata:{str(userdata)} and message id:{mid}")
+
+def main():
+    client = mqtt.Client(userdata=client_data)
+    client.on_publish = on_publish  # callback associated function
+    client.connect(broker, port, keepalive)
+
+    # === Boucle pour generer des données alèatoires de temperature===
+    while True:
+        temp = round(random.randint(22, 30))
+        temp =str(temp)+"°C"
+        client.publish(topic, temp)
+        print(f"Température publiée sur {topic}:{temp}")
+        time.sleep(4)
+
+if __name__ == "__main__":
+    main()
+
+
+
